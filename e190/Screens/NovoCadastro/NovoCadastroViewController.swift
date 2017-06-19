dc95a7070d60afb37cc0d2f9f157a7882eda569a
alprado_brq <alprado@brq.com>
2017-06-19 17:02:21 -0300

    tela de novo cadastro

---
+++ e190/Screens/NovoCadastro/NovoCadastroViewController.swift
@@ -0,0 +1,43 @@
+//
+//  NovoCadastroViewController.swift
+//  e190
+//
+//  Created by Marcelo Pavani on 19/06/17.
+//  Copyright © 2017 GNTEC. All rights reserved.
+//
+
+import UIKit
+
+class NovoCadastroViewController: UIViewController, UIScrollViewDelegate {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //scrollView.delegate = self
+    }
+
+//    override func viewDidAppear(_ animated: Bool) {
+//        scrollView.contentSize = CGSize(width: 306, height: 400.0)
+//    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
